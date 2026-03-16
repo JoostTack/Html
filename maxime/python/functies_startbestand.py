d6e982ed5c077d9da2f781ac470a7177219dd415
Maxime Debosschere <maxime.debosschere@penta-services.be>
2026-03-16 15:36:30 +0100

    Python: startbestand les 4

---
+++ maxime/python/functies_startbestand.py
@@ -0,0 +1,23 @@
+# print(is_even(-2))  # True
+# print(is_even(-1))  # False
+# print(is_even(0))   # True
+# print(is_even(1))   # False
+# print(is_even(2))   # True
+
+
+
+
+# print(gemiddelde([0, 1]))           # 0.5
+# print(gemiddelde([0, 1, 1]))        # 0.6666666666666666
+# print(gemiddelde([0, 1, 2, 3, 4]))  # 2.0
+# print(gemiddelde([-3, 8, 10]))      # 5.0
+# print(gemiddelde([6]))              # 6.0
+
+
+
+
+# print(aantal_klinkers("python"))       # 1
+# print(aantal_klinkers("Informatica"))  # 5
+# print(aantal_klinkers("AEIOU"))        # 5
+# print(aantal_klinkers("xyz"))          # 0
+# print(aantal_klinkers(""))             # 0
